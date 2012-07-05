af589ec7afb1b2576923d5d4e524d4deaaf6511d
WuPeng <null.wp@gmail.com>
2012-07-05 11:37:02 +0800

    GAE下jdoTemplate查询失效，可能是gae不支持springjdo，所以还是使用原生的query

--- src/com/wupeng/blog/dao/jdoimp/UserTestDao.java
+++ src/com/wupeng/blog/dao/jdoimp/UserTestDao.java
@@ -16,10 +16,15 @@ import org.springframework.transaction.annotation.Transactional;
 import com.wupeng.blog.dao.IUserDao;
 import com.wupeng.blog.vo.UserDTO;
 
+/**
+ * @author WuPeng
+ * @date 2012.7.3
+ * @description 通过Spring注入的Userdao
+ */
 @Repository
 @Transactional(readOnly = false)
-public class UserTestDao extends JdoDaoSupport implements IUserDao {
-
+public class UserTestDao extends JdoDaoSupport implements IUserDao 
+{
 	@Override
 	@Transactional
 	public boolean saveUser(UserDTO user) {
@@ -27,21 +32,42 @@ public class UserTestDao extends JdoDaoSupport implements IUserDao {
 		return true;
 	}
 
+
 	@Override
 	@Transactional(readOnly=true)
 	 @SuppressWarnings("unchecked")
 	public List<UserDTO> getAllUser() 
 	{
-		return (List<UserDTO>)getJdoTemplate().executeFind(new JdoCallback() {
-			
-			@Override
-			public Object doInJdo(PersistenceManager pm) throws JDOException {
-				Query query = pm.newQuery(UserDTO.class);
-				query.setRange(0,5);
-				Object result =  query.execute();	
-				return result ;
-			}
-		});
+		Query query = getPersistenceManager().newQuery(UserDTO.class);
+		query.setOrdering("loginName DESC");
+		query.setRange(0,5);
+		Object result = query.execute();
+		query.close(result);
+		
+		return (result == null)?null:(List<UserDTO>)result;	
+		/*
+		 * 直接使用jdoTemplate来查询，结构为空。但是通过获得PersistenceManager来做查询则没有问题，
+		 * 我想，这应该是和jdoTemplate有关，确切的说应该是和Spring对jdo的整合有关，gae可能不支持
+		 * 但是也发现一个有意思的问题，使用jdoTemplate同时使用打断点，则正常
+		 */
+		
+//		return (List<UserDTO>)getJdoTemplate().executeFind(new JdoCallback() {
+//			
+//			@Override
+//			public Object doInJdo(PersistenceManager pm) throws JDOException {
+//				//Query query = pm.newQuery(UserDTO.class);
+//				
+//				String jdoSql = "SELECT FROM com.wupeng.blog.vo.UserDTO";
+//				Query query = pm.newQuery(jdoSql);
+//				Object result = query.execute();
+//				
+////				query.setOrdering("loginName DESC");
+////				query.setRange(0,5);
+////				Collection result =  (Collection)query.execute();	
+////				query.close(result);
+//				return result ;
+//			}
+//		});
 	}
 	
 }
