a8411fd06339ade923e02dac1156c68fe4ed2008
WuPeng <null.wp@gmail.com>
2012-07-05 09:44:14 +0800

    新增单元测试部分，junit.

--- test/com/wupeng/blog/UserDaoTest.java
+++ test/com/wupeng/blog/UserDaoTest.java
@@ -1,5 +1,21 @@
 package com.wupeng.blog;
 
-public class UserDaoTest {
+import junit.framework.TestCase;
 
+import org.junit.Test;
+
+public class UserDaoTest extends TestCase
+{
+	public void testAdd()
+	{
+		int x = 10;
+		int y = 20;
+		System.out.println("x+y="+(x+y));
+		assertEquals(30, x+y+1);
+	}
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello,World!");
+	}
 }
